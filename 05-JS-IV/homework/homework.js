28e039360fb4bdbb86357f577c6587ffbf871e99
Jorge <jorge.estefan98@gmail.com>
2022-10-10 20:14:44 -0500

    Add IV and V

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -10,7 +10,7 @@ function crearGato (nombre, edad) {
     nombre: nombre,
     edad: edad,
     meow: function(){
-      return "Tomillo hizo: Meow!";
+      return "Meow!";
     }
   }
   return gato;
